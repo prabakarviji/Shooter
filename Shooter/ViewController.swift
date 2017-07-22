1a12fd558a4ca488e231d7ec1726fa19c0a5d2f1
prabakarviji <prabakarviji@gmail.com>
2017-07-22 11:28:57 +0530

    Initial Commit

---
+++ Shooter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shooter
+//
+//  Created by Spritle Mac on 22/07/17.
+//  Copyright © 2017 Spritle Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
