5b31c089001ee2c93e195a9e93d3579a0ae57d06
prabakarviji <prabakarviji@gmail.com>
2017-07-25 14:54:43 +0530

    table view added

---
+++ LinkTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LinkTableViewCell.swift
+//  Shooter
+//
+//  Created by Spritle on 25/07/17.
+//  Copyright © 2017 Spritle Mac. All rights reserved.
+//
+
+import UIKit
+
+class LinkTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
